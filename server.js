daab470b7a5c7b63b808c51341370d758e74823a
Irawan Firmansyah <irawanfrmnsyh@gmail.com>
2021-05-21 14:06:00 +0700

    update server.js

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ const path = require("path");
 const app = require("./app");
 require("./database");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
-
-server.on("close", () => {
-  // remove database
-  const dbPath = path.join(__dirname, "db.sqlite");
-  fs.unlink(dbPath, (err) => {
-    if (err) throw err;
-  });
-});
